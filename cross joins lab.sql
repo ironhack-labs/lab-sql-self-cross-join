dc05bfff36bfac10f473c0c8f8a2c7de608bb16a
aidagarcd <aidagarcd@gmail.com>
2024-01-11 19:08:19 +0100

    aida cross joins lab

---
+++ cross joins lab.sql
@@ -0,0 +1,3 @@
+-- 1 Get all pairs of actors that worked together.
+-- 2 Get all pairs of customers that have rented the same film more than 3 times.
+-- 3 Get all possible pairs of actors and films.
